e67e5cc2f8d61376f04045b7ec63593e5ba50dc9
lethalpaga <lethalpaga@gmail.com>
2017-04-19 17:31:42 +1200

    Mask password and add required context

--- vars/github.groovy
+++ vars/github.groovy
@@ -1,15 +1,3 @@
-// class github implements Serializable {
-//   private String githubUrl
-  
-//   def setUrl(url) {
-//     this.githubUrl = url
-//   }
-  
-//   def createDeployment(target) {
-//     echo "Deploying to ${target}"
-//   }
-// }
-
 def getGitCommit() {
   sh returnStdout:true, script: "git rev-parse HEAD"
 }
@@ -26,44 +14,48 @@ def setPassword(passwd) {
   githubPassword = passwd
 }
 
+@NonCPS
+def toJson(payload){
+  new JSON(payload)
+}
+
 def sendPayload(url, payload) {
-  writeFile file: 'payload.json', text: payload
+  echo "Sending payload to ${url}"
+  writeJSON file: 'payload.json', json: toJson(json_payload)
   payload = null
-  sh "curl -f -X POST -u ${githubUser}:${githubPassword} ${url} -d @payload.json \
-              -H 'X-GitHub-Media-Type: application/vnd.github.ant-man-preview+json' > response.json"
+  json_payload = null
+  sh """
+  set +x
+  curl -f -X POST -u ${githubUser}:${githubPassword} ${url} -d @payload.json \
+              -H 'X-GitHub-Media-Type: application/vnd.github.ant-man-preview+json' > response.json
+  set -x
+  """
   readJSON file: 'response.json'
 }
   
-def createDeployment(target, task = "deploy", description = "") {
-  def rev = getGitCommit()
+def createDeployment(target, task = "deploy", description = "", required_contexts = []) {
+  def rev = getGitCommit().trim()
   echo "Deploying rev ${rev} to ${target}"
   
-  def payload = """
-    {
-      "ref": "${rev}",
-      "task": "${task}",
-      "environment": "${target}",
-      "description": "${description}"
-    }
-  """
+  def payload = [
+    ref: "${rev}",
+    task: "${task}",
+    environment: "${target}",
+    description: "${description}",
+    required_contexts: []
+  ]
   def response = sendPayload("${githubUrl}/deployments", payload)
   echo "response is ${response['url'].inspect()}"
   return response['url']
 }
 
-def createDeploymentStatus(url, state, target_url  = "", description = "", environment_url = "", auto_inactive = true){
+// Create a new deployement status
+// @param url Base url to the deployment as return by createDeployment)
+// @param payload Status parameters map as per https://developer.github.com/v3/repos/deployments/#create-a-deployment-status     
+def createDeploymentStatus(url, payload){
   def rev = getGitCommit()
   echo "Updating status of ${url}"
   
-  def payload = """
-    {
-      "state": "${state}",
-      "log_url": "${target_url}",
-      "description": "${description}",
-      "environment_url": "${environment_url}",
-      "auto_inactive": "${auto_inactive}"
-    }
-  """
   def response = sendPayload("${url}/statuses", payload)
   return response['url']
 }
