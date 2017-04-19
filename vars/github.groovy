8b20a531e766b498b0512a290f93229e36cce16a
lethalpaga <lethalpaga@gmail.com>
2017-04-20 10:44:34 +1200

    Better payload implementation

--- vars/github.groovy
+++ vars/github.groovy
@@ -1,5 +1,7 @@
+import groovy.json.JsonOutput
+
 def getGitCommit() {
-  sh returnStdout:true, script: "git rev-parse HEAD"
+  sh(returnStdout:true, script: "git rev-parse HEAD").trim()
 }
 
 def setUrl(url) {
@@ -14,47 +16,31 @@ def setPassword(passwd) {
   githubPassword = passwd
 }
 
-@NonCPS
-def toJson(payload){
-  new JSON(payload)
-}
-
 def sendPayload(url, payload) {
-  echo "Sending payload to ${url}"
-  writeJSON file: 'payload.json', json: toJson(json_payload)
-  payload = null
-  json_payload = null
+  writeFile file: 'payload.json', text: groovy.json.JsonOutput.toJson(payload)
   sh """
   set +x
-  curl -f -X POST -u ${githubUser}:${githubPassword} ${url} -d @payload.json \
+  curl -f -X POST -u '${githubUser}:${githubPassword}' '${url}' -d @payload.json \
               -H 'X-GitHub-Media-Type: application/vnd.github.ant-man-preview+json' > response.json
   set -x
   """
   readJSON file: 'response.json'
 }
   
-def createDeployment(target, task = "deploy", description = "", required_contexts = []) {
-  def rev = getGitCommit().trim()
-  echo "Deploying rev ${rev} to ${target}"
-  
-  def payload = [
-    ref: "${rev}",
-    task: "${task}",
-    environment: "${target}",
-    description: "${description}",
-    required_contexts: []
-  ]
+// Create a new deployement
+// @params Hash containing the parameter as per https://developer.github.com/v3/repos/deployments/#create-a-deployment
+def createDeployment(Map payload) {
   def response = sendPayload("${githubUrl}/deployments", payload)
   echo "response is ${response['url'].inspect()}"
   return response['url']
 }
 
-// Create a new deployement status
+// Send a new deployement status
 // @param url Base url to the deployment as return by createDeployment)
 // @param payload Status parameters map as per https://developer.github.com/v3/repos/deployments/#create-a-deployment-status     
-def createDeploymentStatus(url, payload){
+def createDeploymentStatus(url, Map payload){
   def rev = getGitCommit()
-  echo "Updating status of ${url}"
+  echo "Updating status of ${url} with ${payload}"
   
   def response = sendPayload("${url}/statuses", payload)
   return response['url']
