8e0d173d9947212fbc48c26bc3d7ce5b5022be36
lethalpaga <lethalpaga@gmail.com>
2017-04-20 16:26:08 +1200

    Separate base github url and repo endpoint

--- vars/githubApi.groovy
+++ vars/githubApi.groovy
@@ -16,6 +16,15 @@ def setPassword(passwd) {
   githubPassword = passwd
 }
 
+def setOrg(org) {
+  githubOrg = org
+}
+
+def setRepo(repo) {
+  githubRepo = repo
+}
+
+// Sends an HTTP query to github
 def sendPayload(url, payload) {
   writeFile file: 'payload.json', text: groovy.json.JsonOutput.toJson(payload)
   sh """
@@ -26,11 +35,16 @@ def sendPayload(url, payload) {
   """
   readJSON file: 'response.json'
 }
+
+// Returns the Repos base endpoint
+private githubApiRepoUrl() {
+  "${githubUrl}/repos/${githubOrg}/${githubRepo}"
+}
   
 // Create a new deployement
 // @params Hash containing the parameter as per https://developer.github.com/v3/repos/deployments/#create-a-deployment
 def createDeployment(Map payload) {
-  def response = sendPayload("${githubUrl}/deployments", payload)
+  def response = sendPayload("${githubApiRepoUrl()}/deployments", payload)
   echo "response is ${response['url'].inspect()}"
   return response['url']
 }
@@ -45,3 +59,4 @@ def createDeploymentStatus(url, Map payload){
   def response = sendPayload("${url}/statuses", payload)
   return response['url']
 }
+
